b5ee6cf67fbb2bb1c421e9ea0c89f66f99d3fe66
satwik-08 <110711118+satwik-08@users.noreply.github.com>
2024-09-01 20:17:18 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
         <motion.a
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          href='https://drive.google.com/file/d/1kohwiir4gCXj6BEl2BoyVuyXg9N2-PfL/view?usp=sharing'
+          href='https://drive.google.com/file/d/1yY0Zks0u5C3YxYhOJjGtDd_fS1DAabkE/view?usp=sharing'
           target='_blank'
           className='inline-block mt-8 px-8 py-4 bg-cyan-500 text-neutral-900 font-semibold rounded-full shadow-lg hover:bg-cyan-600 transition duration-300'
         >
